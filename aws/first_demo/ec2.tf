8c42e411e17626fadc6cb735639de7bfdb6d494f
Geethaadmin <97686590+Geethaadmin@users.noreply.github.com>
2022-01-14 19:53:03 +0530

    Update ec2.tf

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -8,13 +8,7 @@ resource "aws_instance" "myawsserver" {
   }
 }
 
-output "myawsserver-ip" {
-  value = aws_instance.myawsserver.public_ip
-}
 
-output "myserver-PrivateIP" {
-  value = aws_instance.myawsserver.private_ip
-}
 
 
 
